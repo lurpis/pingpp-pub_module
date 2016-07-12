e3fc903d287d173c54051a16f86ea4a669c13bde
lurrpis <hi@lurrpis.com>
2016-07-12 13:58:31 +0800

    init

---
+++ src/Hello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author lurrpis
+ * @date 16/7/12 下午1:56
+ * @blog http://lurrpis.com
+ */
+
+namespace pingpp\pub_module;
+
+class Hello
+{
+    public static function fn()
+    {
+        echo 'hello: ' . __METHOD__ . "\n";
+    }
+}
---
+++ src/test/Unit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author lurrpis
+ * @date 16/7/12 下午1:57
+ * @blog http://lurrpis.com
+ */
+
+namespace pingpp\pub_module\test;
+class Unit
+{
+    public static function fn()
+    {
+        echo 'hello: ' . __METHOD__ . "\n";
+    }
+}
